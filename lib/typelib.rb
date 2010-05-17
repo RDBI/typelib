4a1fd3152add2e04b61e12792ccd98db7aec9674
Erik Hollensbe <erik@hollensbe.org>
2010-05-17 18:30:57 -0400

    * Docs!

--- lib/typelib.rb
+++ lib/typelib.rb
@@ -1,15 +1,34 @@
 require 'delegate'
 
+# Typelib is a way of checking and converting data. It operates as a "filter
+# chain" system which allows it to gradually normalize disparate data into a
+# common type. Each chain is optionally a part of a list which allows it to
+# provide several paths in a single external execution.
+#
+# The library is arguably very simple and therefore has simple requirements and
+# needs. This is intentional.
+#
+# Please see TypeLib::Filter and TypeLib::FilterList for more information.
+#
 module TypeLib
 
     VERSION = "0.0.1"
 
+    # A FilterList is a ... list of filters. It includes all the methods that
+    # Array contains, plus an additional method -- execute. See
+    # TypeLib::Filter.
     class FilterList < DelegateClass(Array)
+        # Create a new FilterList. An array of TypeLib::Filter objects is
+        # accepted for construction.
         def initialize(ary=[])
             @filters = ary
             super(@filters)
         end
 
+        # Execute the checks in this list against +obj+, passing in +addl+
+        # if any additional arguments are provided. If the check passes, the
+        # conversion is run and the chain supplied to the constructor is
+        # followed. If no checks pass, the original item is returned.
         def execute(obj, *addl)
             ret = obj 
             @filters.each do |filter|
@@ -22,19 +41,30 @@ module TypeLib
         end
     end
 
+    # TypeLib::Filter is a way of checking, then optionally converting data
+    # based on whether or not the check passes. At that point, an additional
+    # TypeLib::FilterList may be provided which indicates that more checks need
+    # to be followed with the new data.
     class Filter
         attr_reader :check_proc, :convert_proc, :filters
 
+        # A TypeLib::Filter consists of a check procedure, a conversion procedure,
+        # and a TypeLib::FilterList which may be empty. Checks return boolean
+        # which indicates whether or not the conversion will be attempted. 
         def initialize(check, convert, filters=FilterList.new)
             @check_proc   = check 
             @convert_proc = convert
             @filters      = filters
         end
 
+        # Check this object against the filter. If additional data is supplied,
+        # it will be provided to the Filter#check_proc.
         def check(obj, *addl)
             check_proc.call(obj, *addl)
         end
 
+        # Convert this object unconditionally. If additional data is supplied,
+        # it will be provided to the Filter#convert_proc.
         def convert(obj, *addl)
             ret = convert_proc.call(obj, *addl)
             filters.execute(ret, *addl)
--- lib/typelib/canned.rb
+++ lib/typelib/canned.rb
@@ -3,7 +3,19 @@ require 'date'
 require 'typelib'
 
 module TypeLib
+    # Canned checks are just that -- already written for you.
     module Canned
+        #
+        # build_strptime_filter works with DateTime objects and a format that
+        # you provide. It will build and return a new TypeLib::Filter that can
+        # check strings for the format you provide and if they pass, convert
+        # them to a DateTime object.
+        #
+        # Due to limitations in the DateTime system itself, you must include
+        # zone information -- if not, you will get a value that is offset to
+        # GMT, which is not what DateTime.now and pals will return. An
+        # ArgumentError will be raised if you do not supply this in the filter.
+        #
         def build_strptime_filter(format)
             if format !~ /%z/
                 raise ArgumentError, "format must include %z due to DateTime fail"
@@ -18,6 +30,9 @@ module TypeLib
             return Filter.new(check, convert)
         end
 
+        module_function :build_strptime_filter
+
+        # Canned Checks.
         module Checks
             STR_IS_INT = proc { |obj| obj.kind_of?(String) and obj =~ /^\d+$/ }
             STR_IS_DEC = proc { |obj| obj.kind_of?(String) and obj =~ /^[\d.]+$/ }
@@ -26,6 +41,7 @@ module TypeLib
             IS_INTEGER = proc { |obj| obj.kind_of?(Integer) }
         end
 
+        # Canned Conversions.
         module Conversions
             TO_INTEGER         = proc { |obj| obj.to_i }
             TO_FLOAT           = proc { |obj| obj.to_f }
@@ -36,6 +52,7 @@ module TypeLib
             STR_TO_BIGDECIMAL  = proc { |obj| BigDecimal.new(obj.to_s) }
         end
 
+        # Fully canned filters.
         module Filters
             STR_TO_INT   = Filter.new(Checks::STR_IS_INT, Conversions::TO_INTEGER)
             STR_TO_FLOAT = Filter.new(Checks::STR_IS_DEC, Conversions::TO_FLOAT)
